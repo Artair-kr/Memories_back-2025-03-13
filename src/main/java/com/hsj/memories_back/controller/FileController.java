40ac3f9811ec8012222d417ca679556d6cccfa65
Artair-kr <hjs6855@naver.com>
2025-03-25 11:16:03 +0900

    2025-03-25 11:20

--- src/main/java/com/hsj/memories_back/controller/FileController.java
+++ src/main/java/com/hsj/memories_back/controller/FileController.java
@@ -9,7 +9,12 @@ import com.hsj.memories_back.service.FileService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 @RequestMapping("/file")
@@ -26,5 +31,13 @@ public class FileController {
     String url = fileService.upload(file);
     return url;
   }
-  
+
+  // 반환 파일 타입 변경
+  @GetMapping(value="/{fileName}", produces={MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
+  public Resource getImageFile(
+    @PathVariable("filename") String fileName
+  ) { 
+    Resource resource = fileService.getImageFile(fileName);
+    return resource;
+  }
 }
--- src/main/java/com/hsj/memories_back/service/FileService.java
+++ src/main/java/com/hsj/memories_back/service/FileService.java
@@ -1,8 +1,11 @@
 package com.hsj.memories_back.service;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
     
   String upload(MultipartFile file);
+  Resource getImageFile(String fileName);
+
 }
